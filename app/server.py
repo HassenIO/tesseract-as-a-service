be18c25bfcfb88442af503ea535693d3aa84113a
Hassen Taidirt <hassen@hey.com>
2022-03-01 21:28:50 +0100

    Add Tesseract text extraction in the server app

--- app/server.py
+++ app/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from werkzeug.utils import secure_filename
+import pytesseract
 
 
 app = Flask(__name__)
@@ -10,9 +11,11 @@ def run_ocr():
 	f = request.files['file']
 	file_name = 'tmp/' + secure_filename(f.filename)
 	f.save(file_name)
-	return f'hello world = {file_name}'
+
+	text = pytesseract.image_to_string(file_name, timeout=10)
+	return f'text={text}'
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host="0.0.0.0", port=5000, debug=True)
 
