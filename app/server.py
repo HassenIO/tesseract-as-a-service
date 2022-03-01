72e8d95ee642bf811641bba0e77c678e82829b6d
Hassen Taidirt <hassen@hey.com>
2022-03-01 20:19:10 +0100

    Upload file into the tmp folder before processing

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
@@ -6,7 +7,10 @@ app = Flask(__name__)
 
 @app.route('/ocr', methods=['POST'])
 def run_ocr():
-	return 'hello world'
+	f = request.files['file']
+	file_name = 'tmp/' + secure_filename(f.filename)
+	f.save(file_name)
+	return f'hello world = {file_name}'
 
 
 if __name__ == '__main__':
